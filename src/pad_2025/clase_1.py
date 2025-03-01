baf58979ed55d66db5739f4ee919a742dfed3388
Your Name <yenny.gordillo@est.iudigital.edu.co>
2025-03-01 12:15:15 -0500

    clase_1 - progranacion orientada a objetos

---
+++ src/pad_2025/clase_1.py
@@ -0,0 +1,16 @@
+
+
+
+class Personas():
+    def __init__(self):
+        self.nombre = "Paola"
+        self.edad = 0
+        self.estatura = 100.0
+        self.peso = 35.6
+        
+
+
+persona =Personas()
+persona.edad = 42
+persona.estatura = 1.60
+print("Nombre:",persona.nombre,"\n","Edad:",persona.edad,"\n","Estatura:",persona.estatura,"\n")
