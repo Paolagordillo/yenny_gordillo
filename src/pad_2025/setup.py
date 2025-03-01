70d25c37a1091d62bd133ebe16f33f4a6abf5f02
Your Name <yenny.gordillo@est.iudigital.edu.co>
2025-03-01 13:56:42 -0500

    cambion de ruta

---
+++ src/pad_2025/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name= "pad",
+    version="0.0.1",
+    author="Paola Gordillo",
+    author_email="yenny.gordillo@est.iudigital.edu.co",
+    description="configuracion pad",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas"
+        "matplotlib"
+    ],
+    
+)
